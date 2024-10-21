2878a870612cdb6d9d66758a3f936d76bffbe8cd
DaviQzR <132705654+FelypeDantas@users.noreply.github.com>
2024-10-21 10:31:14 -0300

    Criando Cards Dinamicamente

---
+++ scripts/cards.js
@@ -0,0 +1,34 @@
+const projetos = [
+    {
+        titulo: "Relógio",
+        descricao: "Projeto Front-End criado com HTML, CSS e JavaScript, a cor que escolhe é salva no host pelo localhost uma bela junção de dois projetos, palheta de cores e relógio digital.",
+        imagem: "./img/projetos/Relogio.png",
+        link: "https://felypedantas.github.io/Rel-gio_JS/index.html"
+    },
+    {
+        titulo: "Portfólio Cartão",
+        descricao: "Projeto Front-End criado com Tailwind, este projeto se trata do treino de uma habilidade que aprendi nos cursos da Alura, usando o Tailwind para criar um cartão com links de acesso a algumas de minhas mídias sociais.",
+        imagem: "./img/projetos/cartao.png",
+        link: "https://felypedantas.github.io/Tailwind_Cartao/"
+    },
+    // Adicione os outros projetos aqui...
+];
+
+const cardsContainer = document.getElementById('cards-container');
+
+projetos.forEach(projeto => {
+    const card = document.createElement('div');
+    card.className = 'card m-4';
+    card.style.width = '20rem';
+
+    card.innerHTML = `
+        <img src="${projeto.imagem}" class="card-img-top" alt="Projeto ${projeto.titulo}">
+        <div class="card-body">
+            <h5 class="card-title py-2 fw-bold">${projeto.titulo}</h5>
+            <p class="card-text">${projeto.descricao}</p>
+            <a href="${projeto.link}" class="btn botao-padrao w-100 fw-bold">Quero detalhes</a>
+        </div>
+    `;
+
+    cardsContainer.appendChild(card);
+});
