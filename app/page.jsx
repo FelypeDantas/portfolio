7be1a2657248e026e2964ba76a08890ce20e5dab
Felype <felyped03@gmail.com>
2024-11-04 08:54:02 -0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -16,10 +16,12 @@ const Home = () => {
               Estou sempre em busca de novos desafios e oportunidades de crescimento.
               Vamos construir algo incrível juntos!</p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
+              <a href="assets/Curriculo_Felype.pdf" download >
               <Button variant="outline" size="lg" className="uppercase flex itens-center gap-2">
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
+              </a>
               <div className="mb-8 xl:mb-0">
                 <Social containerStyles="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
               </div>
