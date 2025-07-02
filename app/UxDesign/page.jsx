76d7382231990f24267bf7b88c42844d62ae7c0f
Felype <felyped03@gmail.com>
2025-07-01 21:40:37 -0300

    Criação de página para UX

--- app/UxDesign/page.jsx
+++ app/UxDesign/page.jsx
@@ -4,34 +4,25 @@ import {BsArrowDownRight} from 'react-icons/bs';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
-const services = [
+const uxdesign = [
     {
         num: '01',
-        title: 'Web Development',
-        description: 'Desenvolvedor web cria e mantém sites, garantindo que sejam funcionais, responsivos e esteticamente agradáveis. Trabalha com linguagens de programação, design e experiência do usuário para otimizar a presença online.',
-        href: 'https://portfolio-umber-seven-61.vercel.app/work'
-    },
-    {
-        num: '02',
-        title: 'UI/UX Design',
-        description: 'UI/UX Design envolve a criação de interfaces intuitivas e agradáveis para usuários, focando na usabilidade e na experiência do cliente. O objetivo é tornar produtos digitais mais acessíveis e envolventes, melhorando a interação entre usuários e sistemas.',
-        href: '/uxdesign'
-    },
-    {
-        num: '03',
-        title: 'Logo Design',
-        description: 'O design de logotipos envolve a criação de identidades visuais únicas e memoráveis para marcas. Um bom logotipo transmite os valores da empresa e estabelece uma conexão emocional com o público.',
+        title: 'Canva',
         href: ''
     },
     {
-        num: '04',
-        title: 'Back-End Developer',
-        description: 'Um desenvolvedor back-end é responsável pela lógica do servidor, bancos de dados e integração de sistemas, garantindo que as funcionalidades de um site ou aplicativo funcionem corretamente. Ele lida com a parte "invisível" da tecnologia, otimizando desempenho e segurança.',
+        num: '02',
+        title: 'Figma',
+        href: ''
+    },
+    {
+        num: '03',
+        title: 'Adobe Photoshop',
         href: ''
     }
 ]
 
-const Services = () => {
+const Uxdesign = () => {
     return (
         <section className='min-h-[80vh] flex flex-col justify-center py-12 xl:py-0'>
             <div className="container mx-auto">
@@ -39,7 +30,7 @@ const Services = () => {
                  initial={{opacity: 0}} 
                  animate={{opacity: 1, transition: {delay: 2.4, duration: 0.4, ease: 'easeIn'},
             }} className="grid grid-cols-1 md:grid-cols-2 gap-[60px]">
-                    {services.map((service, index) => {
+                    {uxdesign.map((service, index) => {
                        return <div className='flex-1 flex flex-col justify-center gap-6 group' key={index}>
                         <div className='w-full flex justify-between items-center'>
                             <div className='text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500'>{service.num}</div>
@@ -58,4 +49,4 @@ const Services = () => {
     )
 }
 
-export default Services;
+export default Uxdesign;
