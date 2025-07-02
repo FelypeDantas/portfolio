ebfb7b9a74d78a0030e9e7ea4468f3f4833ec62e
Felype <felyped03@gmail.com>
2025-07-02 19:51:06 -0300

    correção na pasta canva

--- app/canva/page.jsx
+++ app/canva/page.jsx
@@ -1,69 +1,81 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React, {useState} from "react";
-import {Swiper, SwiperSlide} from "swiper/react";
-import 'swiper/css';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import Image from "next/image";
 import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
-    {
-        num: "01",
-        title: "Projeto 1",
-        description: "Um post de Instagram criado no Canva, com foco em criar uma identidade visual marcante e atraente.",
-        image: "/assets/canva/VivaLeve.png"
-    },
-
+  {
+    num: "01",
+    title: "Projeto 1",
+    description:
+      "Um post de Instagram criado no Canva, com foco em criar uma identidade visual marcante e atraente.",
+    image: "/assets/canva/VivaLeve.png",
+  },
 ];
 
-const Canvas = () => {
-    const [project, setProjects] = useState(projects[0]);
-    const handleSlideChange = (swiper) => {
-        const currentIndex = swiper.activeIndex;
-        setProjects(projects[currentIndex])
-    }
-    return(
-        <motion.section initial={{opacity: 0}} animate={{opacity:1, transition: {delay: 2.4 , duration: 0.4, ease: 'easeIn'}, }} className="min-h-[80vh] flex flex-col justify-center py-12 px-0">
-            <div className="container mx-auto">
-                <div className="flex flex-col xl:flex-row xl:gap-[30px]">
-                    <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-                        <div className="flex flex-col gap-[30px] h-[50%]">
-                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">{project.num}</div>
-                            <p className="text-white/60">{project.description}</p>
-                            <ul className="flex gap-4">
-                                {project.stack.map((item, index) => {
-                                    return <li key={index} className="text-xl text-accent ">
-                                        {item.name}
-                                        {index !== project.stack.length -1 && ","}
-                                    </li>
-                                })}
-                            </ul>
-                            <div className="border border-white/20 "></div>
-                        </div>
-                    </div>
-                    <div className="w-full xl:w-[50%]">
-                        <Swiper spaceBetween={30} slidesPerView={1} className="xl:h-[520px] mb-12" onSlideChange={handleSlideChange}>
-                            {projects.map((project, index) => {
-                                return (<SwiperSlide key={index} className="w-full ">
-                                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                                        <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                                        <div className="relative w-full h-full">
-                                            <Image src={project.image} fill className="object-cover" alt="Projeto"/>
-                                        </div>
-                                    </div>
-                                </SwiperSlide>);
-                            })}
-                            <WorkSliderBtns
-                            containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                            btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-                            />
-                        </Swiper>
-                    </div>
-                </div>
+const CanvaProjects = () => {
+  const [selectedProject, setSelectedProject] = useState(projects[0]);
+
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setSelectedProject(projects[currentIndex]);
+  };
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 2.4, duration: 0.4, ease: "easeIn" }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="flex flex-col gap-[30px] h-[50%]">
+              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                {selectedProject.num}
+              </div>
+              <p className="text-white/60">{selectedProject.description}</p>
+              {/* Removido o trecho de stack.map */}
+              <div className="border border-white/20 "></div>
             </div>
-        </motion.section>
-    );
+          </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => (
+                <SwiperSlide key={index} className="w-full">
+                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={project.image}
+                        fill
+                        className="object-cover"
+                        alt={project.title}
+                      />
+                    </div>
+                  </div>
+                </SwiperSlide>
+              ))}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
-export default Canvas;
+export default CanvaProjects;
