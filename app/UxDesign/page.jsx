ee551b2b347067d1b176101788fb2b33b3ce29e0
Felype <felyped03@gmail.com>
2025-07-02 19:19:48 -0300

    exclusão de descrição

--- app/UxDesign/page.jsx
+++ app/UxDesign/page.jsx
@@ -39,7 +39,6 @@ const Uxdesign = () => {
                             </Link>
                         </div>
                         <h2 className='text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500'>{service.title}</h2>
-                        <p className='text-white/60'>{service.description}</p>
                         <div className='border-b border-white/20 w-full'></div>
                        </div>
                     })}
