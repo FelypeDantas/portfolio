a5759d7195befd6deb9d3bce250329ad46cc7e91
Felype <felyped03@gmail.com>
2025-07-04 20:18:01 -0300

    colocando link para as redes

--- app/canva/page.jsx
+++ app/canva/page.jsx
@@ -2,6 +2,8 @@
 
 import { motion } from "framer-motion";
 import React, { useState } from "react";
+import Link from "next/link";
+import { BsArrowUpRight } from "react-icons/bs";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import Image from "next/image";
@@ -11,9 +13,9 @@ const projects = [
   {
     num: "01",
     title: "Projeto 1",
-    description:
-      "Um post de Instagram criado no Canva, com foco em criar uma identidade visual marcante e atraente.",
+    description: "Um post de Instagram criado no Canva, com foco em criar uma identidade visual marcante e atraente.",
     image: "/assets/canva/VivaLeve.png",
+    live: "https://www.instagram.com/p/DLfYwJVAWkeN57mAMIUF-dp6KGVtEpOD1-bEUg0/"
   },
 ];
 
@@ -42,38 +44,52 @@ const CanvaProjects = () => {
               <p className="text-white/60">{selectedProject.description}</p>
               {/* Removido o trecho de stack.map */}
               <div className="border border-white/20 "></div>
+              <div className="flex items-center gap-4">
+                <Link href={selectedProject.live}>
+                  <TooltipProvider delayDuration={100}>
+                    <Tooltip>
+                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                        <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
+                      </TooltipTrigger>
+                      <TooltipContent>
+                        <p>Projeto nas redes</p>
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                </Link>
+                </div>
+              </div>
+            </div>
+            <div className="w-full xl:w-[50%]">
+              <Swiper
+                spaceBetween={30}
+                slidesPerView={1}
+                className="xl:h-[520px] mb-12"
+                onSlideChange={handleSlideChange}
+              >
+                {projects.map((project, index) => (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full">
+                        <Image
+                          src={project.image}
+                          fill
+                          className="object-cover"
+                          alt={project.title}
+                        />
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                ))}
+                <WorkSliderBtns
+                  containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                  btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                />
+              </Swiper>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]">
-            <Swiper
-              spaceBetween={30}
-              slidesPerView={1}
-              className="xl:h-[520px] mb-12"
-              onSlideChange={handleSlideChange}
-            >
-              {projects.map((project, index) => (
-                <SwiperSlide key={index} className="w-full">
-                  <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                    <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
-                    <div className="relative w-full h-full">
-                      <Image
-                        src={project.image}
-                        fill
-                        className="object-cover"
-                        alt={project.title}
-                      />
-                    </div>
-                  </div>
-                </SwiperSlide>
-              ))}
-              <WorkSliderBtns
-                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
-              />
-            </Swiper>
-          </div>
         </div>
-      </div>
     </motion.section>
   );
 };
