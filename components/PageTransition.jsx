61b41728027a8118e25d7b30e7694092ec750785
Felype <felyped03@gmail.com>
2025-12-14 20:03:27 -0300

    removendo animate não usado

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { animate, AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
 const PageTransition = ({children}) => {
