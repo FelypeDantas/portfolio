b27ad89026a80cebc8368d57dcde4083dbf22f13
Felype <felyped03@gmail.com>
2026-04-27 16:07:23 -0300

    Update Social.jsx

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,23 +1,57 @@
-"use client"
+"use client";
 
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn, FaYoutube, FaInstagram} from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaYoutube,
+  FaInstagram,
+} from "react-icons/fa";
 
 const socials = [
-    {icon: <FaGithub />, path: "https://github.com/FelypeDantas"},
-    {icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/felype-dantas-dos-santos-94497b193"},
-    {icon: <FaYoutube />, path: "https://www.youtube.com/channel/UCnJ4rJGj0wxeDm2aGSYsxQA"},
-    {icon: <FaInstagram />, path: "https://instagram.com/felype_dantas_santos"}
+  {
+    id: "github",
+    icon: FaGithub,
+    href: "https://github.com/FelypeDantas",
+    label: "GitHub",
+  },
+  {
+    id: "linkedin",
+    icon: FaLinkedinIn,
+    href: "https://www.linkedin.com/in/felype-dantas-dos-santos",
+    label: "LinkedIn",
+  },
+  {
+    id: "youtube",
+    icon: FaYoutube,
+    href: "https://www.youtube.com/channel/UCnJ4rJGj0wxeDm2aGSYsxQA",
+    label: "YouTube",
+  },
+  {
+    id: "instagram",
+    icon: FaInstagram,
+    href: "https://instagram.com/felype_dantas_santos",
+    label: "Instagram",
+  },
 ];
 
-const Social = ({containerStyles, iconStyles}) => {
-    return (
-        <div className={containerStyles}>
-            {socials.map((item, index) => {
-                return <Link key={index} href={item.path} className={iconStyles}>{item.icon}</Link>
-            })}
-        </div>
-    )
-}
+const Social = ({ containerStyles = "", iconStyles = "" }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map(({ id, icon: Icon, href, label }) => (
+        <Link
+          key={id}
+          href={href}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={label}
+          className={iconStyles}
+        >
+          <Icon />
+        </Link>
+      ))}
+    </div>
+  );
+};
 
 export default Social;
