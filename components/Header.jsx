bab36df0ad84362da2fe3d74a1f52800521efedf
Felype <felyped03@gmail.com>
2026-04-27 16:21:05 -0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,25 +4,29 @@ import Nav from "./Nav";
 import MobileNav from "./MobileNav";
 
 const Header = () => {
-    return (
-        <header className="py-8 xl:py-12 text-white">
-            <div className="conatiner mx-auto flex justify-between items-center">
-                <Link href="/">
-                    <h1 className="text-4xl font-semibold">Felype<span className="text-accent">.</span></h1>
-                </Link>
-                <div className="hidden xl:flex items-center gap-8">
-                    <Nav />
-                    <Link href="/contact">
-                        <Button>Contrate-me</Button>
-                    </Link>
-                </div>
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <Link href="/" className="text-4xl font-semibold">
+          Felype<span className="text-accent">.</span>
+        </Link>
 
-                <div className="xl:hidden">
-                    <MobileNav />
-                </div>
-            </div>
-        </header>
-    )
-}
+        {/* Desktop Navigation */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Button asChild>
+            <Link href="/contact">Contrate-me</Link>
+          </Button>
+        </div>
+
+        {/* Mobile Navigation */}
+        <div className="xl:hidden">
+          <MobileNav />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
